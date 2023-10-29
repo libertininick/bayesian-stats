b267028bb7d26dae698a9505fdb681286446e3d8
libertininick <libertini.nick@gmail.com>
2023-10-29 14:40:53 -0600

    Add mcmc.CumulativeDistribution

--- src/bayesian_stats/mcmc.py
+++ src/bayesian_stats/mcmc.py
@@ -27,6 +27,92 @@ class Bounds(NamedTuple):
     upper: float
 
 
+class CumulativeDistribution:
+    """Estimate of the cumulative probability distribution for a set of \
+        parameter samples."""
+
+    def __init__(self, samples: Tensor) -> None:
+        """Initialize from a set of parameter samples.
+
+        Parameters
+        ----------
+        samples: Tensor, shape=(num_samples, num_parameters)
+            Samples for each parameter.
+        """
+        if samples.ndim != 2:
+            raise ValueError(
+                "Expecting a 2D tensor of samples (samples x parameters);"
+                f"got {samples.ndim}D"
+            )
+        self.num_samples, self.num_parameters = samples.shape
+
+        # Sort each column of samples smallest to largest
+        sorted_samples = torch.sort(samples, dim=0)
+
+        # Cumulative probability values
+        self.cumulative_prob = (
+            torch.arange(self.num_samples).to(samples) / self.num_samples
+        )
+        self.cumulative_values: Tensor = sorted_samples.values
+
+        # Store a contiguous, transposed copy of cumulative values for prob lookup
+        self._contig_cvalues = self.cumulative_values.T.contiguous()
+
+    def get_prob(self, values: Tensor) -> Tensor:
+        """Look up the cumulative probability given distribution values.
+
+        Parameters
+        ----------
+        values: Tensor, shape=(N, num_parameters)
+            A set of values for each parameter to look up the cumulative \
+                probability for.
+
+        Returns
+        -------
+        probs: Tensor, shape=(N, num_parameters)
+            Cumulative probability @ values.
+        """
+        if values.ndim != 2 or values.shape[-1] != self.num_parameters:
+            raise ValueError(
+                f"values must have shape (N, {self.num_parameters}); got {values.shape}"
+            )
+        return torch.searchsorted(self._contig_cvalues, values.T).T / self.num_samples
+
+    def get_value(self, probs: Tensor) -> Tensor:
+        """Look up the distribution values for a given set of cumulative probabilities.
+
+        Parameters
+        ----------
+        values: Tensor, shape=(N, num_parameters)
+            A set of values for each parameter to look up the cumulative \
+                probability for.
+
+        Returns
+        -------
+        probs: Tensor, shape=(N, num_parameters)
+            Cumulative probability @ values.
+        """
+        if probs.ndim != 2 or probs.shape[-1] != self.num_parameters:
+            raise ValueError(
+                f"probs must have shape (N, {self.num_parameters}); got {probs.shape}"
+            )
+
+        # Get index of probabilities to look up distribution values with
+        idx = probs * self.num_samples
+        idx_lb = torch.floor(idx).to(torch.long)
+        idx_ub = torch.ceil(idx).to(torch.long)
+
+        # Determine weight for upper bound index based on remainder
+        ub_wt = idx % 1
+
+        # Look up distribution values by index
+        values_lb = torch.gather(self.cumulative_values, dim=0, index=idx_lb)
+        values_ub = torch.gather(self.cumulative_values, dim=0, index=idx_ub)
+
+        # Return weighted average of upper and lower values for indexes
+        return values_lb * (1 - ub_wt) + values_ub * ub_wt
+
+
 def initialize_samples(
     bounds: Iterable[Bounds | tuple[float, float]],
     num_samples: int,
