95cfb6ad1e2598aecb034b3060f8be6d102a7675
libertininick <libertini.nick@gmail.com>
2023-10-30 11:31:32 -0600

    Add mcmc.et_proposal_distribution

--- src/bayesian_stats/mcmc.py
+++ src/bayesian_stats/mcmc.py
@@ -5,11 +5,17 @@ iterations, so at the end of the iterations you have `N` samples approximating
 the posterior distribution.
 """
 import math
-from typing import Callable, Iterable, NamedTuple
+from typing import Callable, Iterable, NamedTuple, ParamSpec
 
+import pyro.distributions as dist
 import torch
 from scipy.stats import qmc
 from torch import Tensor
+
+from bayesian_stats.utils import CumulativeDistribution
+
+
+P = ParamSpec("P")
 
 
 class Bounds(NamedTuple):
@@ -25,6 +31,105 @@ class Bounds(NamedTuple):
 
     lower: float
     upper: float
+
+
+def get_proposal_distribution(
+    samples: Tensor,
+    lower_bounds: Tensor,
+    upper_bounds: Tensor,
+    proposal_width: float,
+) -> dist.Uniform:
+    """Get a uniform proposal distribution for each parameter sample.
+
+    Parameters
+    ----------
+    samples: Tensor, shape=(num_samples, num_parameters)
+        Samples for each parameter.
+    lower_bounds: Tensor, shape=(num_parameters,)
+        Lower bound for each parameter.
+    upper_bounds: Tensor, shape=(num_parameters,)
+        Upper bound for each parameter.
+    proposal_width: float
+        Width of proposal distribution expressed as a percentage of sample \
+            distribution for each parameter.
+
+    Returns
+    -------
+    proposal_dist: pyro.distributions.Uniform, shape=(num_samples, num_parameters)
+        Uniform proposal distribution for each parameter sample.
+    """
+    # Estimate cdf for each parameter from samples
+    cdist = CumulativeDistribution(
+        samples=torch.cat((lower_bounds[None], samples, upper_bounds[None]), dim=0)
+    )
+
+    # Get cumulative dist probability for each sample
+    sample_cprobs = cdist.get_prob(samples)
+
+    # Calculate proposal distribution probability bounds
+    proposal_lb_prob = sample_cprobs - proposal_width / 2
+    proposal_ub_prob = sample_cprobs + proposal_width / 2
+
+    # Shift out of bounds lower bounds
+    oob_mask = proposal_lb_prob < 0
+    proposal_ub_prob += -proposal_lb_prob * oob_mask
+
+    # Shift out of bounds upper bounds
+    oob_mask = proposal_ub_prob > 1
+    proposal_lb_prob -= (proposal_ub_prob - 1) * oob_mask
+
+    # Clip of any remaining oobs
+    proposal_lb_prob = proposal_lb_prob.clip(min=0.0, max=1.0)
+    proposal_ub_prob = proposal_ub_prob.clip(min=0.0, max=1.0)
+
+    # Look up values from probabilities
+    proposal_lb = cdist.get_value(proposal_lb_prob)
+    proposal_ub = cdist.get_value(proposal_ub_prob)
+
+    # Build uniform proposal distribution from bounds
+    proposal_dist = dist.Uniform(low=proposal_lb, high=proposal_ub)
+
+    return proposal_dist
+
+
+def get_sample_log_prob(
+    parameters: list[str],
+    samples: Tensor,
+    prior: Callable[P, Tensor],
+    likelihood: Callable[P, Tensor] | None,
+) -> Tensor:
+    """Get un-normalized log probability of samples from likelihood and prior functions.
+
+    Parameters
+    ----------
+    parameters: list[str], len=num_parameters
+        Model parameter names.
+    samples: Tensor, shape=(num_samples, num_parameters)
+        Samples for each parameter.
+    prior: Callable[..., Tensor]
+        Function that takes parameter samples as Tensors and returns a single Tensor \
+            for the joint prior log probability.
+    likelihood: Callable[..., Tensor] | None
+        Function w/ same parameter signature as `prior`, that takes parameter samples \
+            as Tensors and returns a single Tensor for the joint log likelihood. \
+            If `None`, then only prior joint probability will be returned.
+
+    Returns
+    -------
+    log_probs: : Tensor, shape=(num_samples,)
+        Un-normalized log probability of samples.
+    """
+    # Convert sample tensor to sample dict
+    sample_dict = dict(zip(parameters, samples.T))
+
+    # Get joint prior log probability given parameter samples
+    log_probs = prior(**sample_dict)
+
+    if likelihood is not None:
+        # Add the joint log likelihood given parameter samples
+        log_probs += likelihood(**sample_dict)
+
+    return log_probs
 
 
 def initialize_samples(
@@ -79,39 +184,3 @@ def initialize_samples(
     samples = qmc.scale(samples, l_bounds, u_bounds)
 
     return torch.from_numpy(samples).to(device=device, dtype=dtype)
-
-
-def get_sample_log_prob(
-    parameters: list[str],
-    samples: Tensor,
-    likelihood: Callable[[dict[str, Tensor]], Tensor],
-    prior: Callable[[dict[str, Tensor]], Tensor],
-) -> Tensor:
-    """Get un-normalized log probability of samples from likelihood and prior functions.
-
-    Parameters
-    ----------
-    parameters: list[str], len=num_parameters
-        Model parameter names.
-    samples: Tensor, shape=(num_samples, num_parameters)
-        Samples for each parameter.
-    likelihood: Callable[[dict[str, Tensor]], Tensor]
-        Function that takes parameter samples and returns a log likelihood for
-        each sample.
-    prior: Callable[[dict[str, Tensor]], Tensor]
-        Function that takes parameter samples and returns prior log probability
-        for each sample.
-
-    Returns
-    -------
-    log_probs: : Tensor, shape=(num_samples,)
-        Un-normalized log probability of samples.
-    """
-    # Convert sample tensor to sample dict
-    sample_dict = dict(zip(parameters, samples.T))
-
-    # Get prior probability and likelihood of each sample's parameters
-    prior_log_probs = prior(**sample_dict)
-    log_likelihoods = likelihood(**sample_dict)
-
-    return prior_log_probs + log_likelihoods
--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -1,11 +1,44 @@
 """Test for MCMC module."""
 
+import pyro.distributions as dist
 import pytest
 import torch
+from torch import Tensor
 
-from bayesian_stats.mcmc import initialize_samples
+from bayesian_stats.mcmc import Bounds, get_proposal_distribution, initialize_samples
 
 
+# Test constants
+NUM_SAMPLES = 1_024
+NUM_PARAMETERS = 3
+PROPOSAL_WIDTH = 0.1
+
+
+# Test fixtures
+@pytest.fixture(scope="module")
+def parameter_bounds() -> dict[str, Bounds]:
+    """Get a dictionary of parameter bounds test fixture."""
+    return dict(pi=Bounds(0.0, 1.0), mu=Bounds(-5.0, 2.0), sigma=Bounds(1e-6, 3.0))
+
+
+@pytest.fixture(scope="module")
+def samples(parameter_bounds: dict[str, Bounds]) -> Tensor:
+    """Get parameter samples test fixture."""
+    return initialize_samples(
+        parameter_bounds.values(), num_samples=NUM_SAMPLES, seed=456
+    )
+
+
+@pytest.fixture(scope="module")
+def proposal_dist(samples: Tensor, parameter_bounds: dict[str, Bounds]) -> dist.Uniform:
+    """Get a uniform proposal distribution text fixture."""
+    lower_bounds, upper_bounds = torch.tensor(list(parameter_bounds.values())).T
+    return get_proposal_distribution(
+        samples, lower_bounds, upper_bounds, proposal_width=PROPOSAL_WIDTH
+    )
+
+
+# Tests
 @pytest.mark.parametrize("num_samples", [3, 5, 6, 7])
 def test_initialize_samples_raise_on_bad_num_samples(num_samples: int) -> None:
     """Test that a `num_samples` that isn't a power of 2 raises an exception."""
@@ -27,3 +60,38 @@ def test_initialize_samples_seeded() -> None:
         bounds=[(0.0, 1.0), (-5.0, 2.0)], num_samples=4, seed=1234
     )
     assert torch.allclose(expected, samples)
+
+
+def test_proposal_distribution_shape(proposal_dist: dist.Uniform) -> None:
+    """Test that proposal distribution has correct shape."""
+    assert proposal_dist.shape() == (NUM_SAMPLES, NUM_PARAMETERS)
+
+
+def test_proposal_distribution_width(
+    proposal_dist: dist.Uniform, parameter_bounds: dict[str, Bounds]
+) -> None:
+    """Test that each parameter sample's proposal distribution has correct width."""
+    for i, bounds in enumerate(parameter_bounds.values()):
+        # Samples were randomly initialized so expected sampled dist is ~ U(lb, ub)
+        expected_dist = dist.Uniform(*bounds)
+
+        # Look up cumulative probability of lower and upper bounds of proposal dist
+        lower_prob = expected_dist.cdf(proposal_dist.low[:, i])
+        upper_prob = expected_dist.cdf(proposal_dist.high[:, i])
+
+        # Check width of proposal dist is roughly = PROPOSAL_WIDTH
+        diff = torch.abs((upper_prob - lower_prob) - PROPOSAL_WIDTH)
+        assert torch.all(diff <= 0.01)
+
+
+def test_proposal_distribution_sample(
+    proposal_dist: dist.Uniform, parameter_bounds: dict[str, Bounds]
+) -> None:
+    """Test a sample from the proposal distribution is correct shape and all proposed \
+        values are within bounds."""
+    proposals = proposal_dist.sample()
+    assert proposals.shape == (NUM_SAMPLES, NUM_PARAMETERS)
+
+    lower_bounds, upper_bounds = torch.tensor(list(parameter_bounds.values())).T
+    assert torch.all(proposals >= lower_bounds[None])
+    assert torch.all(proposals <= upper_bounds[None])
